59ab81fdf12e44e1dbf5c709612d6598fb979864
RifaiFirmansyah <rifaifir0987@gmail.com>
2025-10-07 13:57:29 +0700

    tugas saya ni

---
+++ Playlist.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Playlist {
+    private Song head;
+
+    public Playlist() {
+        this.head = null;
+    }
+
+    // tambah lagu di akhir playlist
+    public void addSong(String title) {
+        Song newSong = new Song(title);
+        if (head == null) {
+            head = newSong;
+        } else {
+            Song temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newSong;
+        }
+        System.out.println("Lagu \"" + title + "\" berhasil ditambahkan!");
+    }
+
+    // tampilkan semua lagu
+    public void displayPlaylist() {
+        if (head == null) {
+            System.out.println("Playlist masih kosong!");
+            return;
+        }
+        System.out.println("Daftar Lagu dalam Playlist:");
+        Song temp = head;
+        while (temp != null) {
+            System.out.println("- " + temp.title);
+            temp = temp.next;
+        }
+    }
+
+    // Program utama
+    public static void main(String[] args) {
+        Playlist myPlaylist = new Playlist();
+        myPlaylist.addSong("i love you but im letting go - pamungkas");
+        myPlaylist.addSong("tampar - juicy luicy");
+        myPlaylist.addSong("Bohemian Rhapsody");
+        myPlaylist.displayPlaylist();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan judul lagu baru untuk ditambahkan: ");
+        String newTitle = scanner.nextLine();
+        myPlaylist.addSong(newTitle);
+        myPlaylist.displayPlaylist();
+        scanner.close();
+    }
+}
